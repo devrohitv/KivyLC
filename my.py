dcbdc8d3c3d7aa9bd3203ecba17d1e9a8ce0f8ee
Rohit Vishwakarma <126248971+wish-rohitv@users.noreply.github.com>
2023-09-16 16:44:59 +0530

    initial

---
+++ my.py
@@ -0,0 +1,40 @@
+from kivy.app import App
+from kivy.properties import StringProperty
+from kivy.uix.boxlayout import BoxLayout
+from pyrebase import pyrebase
+import json
+
+with open('serviceAccount.json', 'r') as file_obj:
+    f = json.load(file_obj)
+config = {
+    "apiKey": "AIzaSyAxiO8FrPTUF4I7PSR6i8ZlqXWJMeVyYxQ",
+    "authDomain": "classmate-classes-f9057.firebaseapp.com",
+    "databaseURL": "https://classmate-classes-f9057-default-rtdb.firebaseio.com",
+    "projectId": "classmate-classes-f9057",
+    "storageBucket": "classmate-classes-f9057.appspot.com",
+    "messagingSenderId": "286165438643",
+    "appId": "1:286165438643:web:658c70f981cf5b8cabe0c6",
+    "measurementId": "G-18KW9VP3P6",
+    "serviceAccount": f,
+    "databaseUrl": "https://classmate-classes-f9057-default-rtdb.firebaseio.com/"
+}
+firebase_ = pyrebase.initialize_app(config)
+
+
+class MyData(BoxLayout):
+    source_file = StringProperty("")
+    def show_data(self):
+        storage = firebase_.storage()
+        # storage.child('hello').put('C:/Users/user/Downloads/GITHUBERROR.jpg')
+        storage.download("hello", f"{'file'}/{'my.jpg'}")
+        print("downloaded")
+
+        self.source_file = "file/my.jpg"
+
+
+class MyApp(App):
+    def build(self):
+        return MyData()
+
+
+MyApp().run()
