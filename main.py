23f26556c1e38a9721c7dbb72059ad1c0b877849
Rohit Vishwakarma <126248971+wish-rohitv@users.noreply.github.com>
2023-09-23 18:38:21 +0530

    permission

--- main.py
+++ main.py
@@ -4,22 +4,13 @@ from kivy.properties import StringProperty
 from kivy.uix.boxlayout import BoxLayout
 import firebase_admin
 from firebase_admin import credentials, storage
+from jnius import autoclass
+from android.permissions import check_permission, request_permissions, Permission
 
-d = {
-  "type": "service_account",
-  "project_id": "classmate-classes",
-  "private_key_id": "bb6fe91c517607d1274e1b21195f596bc1a6fbae",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDa4I9JJP/TKyda\n7mjN0i7o/d9+8GqhZy4Wn4aNj5FJiiGjtLj9SpqXVQQ6+e8wyBZO4RQ+1jjoiCm2\ndQD2644dzc3pkspJVUnN2C5LfwV8H8Z1OndICwJg95MgY+lsWXlwZiRQiyyFU4UU\nPOOQ22/Ovm/w0ZtnjMTPny4V9Zgsm9Gm4Z5YxPXv/xxHj7rNGjyJP9RHx2j+Iciq\nr8K10YRIwPcyfhlkaUNIyxiKGS8Oyy4nsanA/7hA7h6fBCKHOdS3ea6QEh8WzhvU\neRgEed421gEdYVLS3+WKus78TM3SODCz2UQeMdvJ4jmEq7caDtUq2cLjU1ukWMvW\ntUAg5BfvAgMBAAECggEABKkec/TFO8Zh9qY39hYBKLXrMeVFp/0t9o5cC5ygf9vg\nnn9FJyOVCh7l0amrxbKlAvX4++OM36qylK+GHzvh0VGmDlCxXQFjam4tgi8Iu11j\n2PAvRQny+YScDecCu+tl0hZitJJaFtax8T3grJ93kIPul0anU/uj3zXpAc+yd1Sp\n+7zakSMJX9tEbWpXpsoVyAPec8yyWeyse7oqQXpdVBsNaJz8KecRUGPmfPHPYFj1\nJ4ssjSF1scg5Varc1GLgV9mTdnItoVt1y2qbADZCBX4Cj4RrBc2jPRQ/IdJ1OwSC\nV/uRX7PXpO2kcruGCVJ7h2KqMqph3wtwBpHF6GuqIQKBgQDv5zJqidCMtBnatJL7\nLQTwsk6OgENve/PzmUS2wchbTBXYIK0iztAMSC9n+WGQMBHweQjcMDmXYbeBoPgC\nzqovvrJ3pzbiW0REyj9wid4VWAE57HSrbxuNVQXas5n7RN8focuHA/pr9k4rbjqm\nLR+XMtrWtOQ0JmlVge0/ytmAIQKBgQDpkDOYVscweg7Ys2hrZimN3LGH4YmiQeBX\nysOQGbZHE+V0bwbYhQu81aRs/l0fWHiTWge4I1iND6iDJXPFmlio91NPfMfHIvxf\n6fT39C+xEXTq3dOyoY8jJ2aVbev03HYTo+j74iJG3kHkvkz4MEGLl0inJRBONm0c\nQexT88rWDwKBgQDtVGkZ4JDCQYG6TR2DArx6acsxSk5sQdHK0Xua2QYe25xYnarq\nXnKPU/IiCCGuhP+aSAKt4b93tSHXSalRDaZraVog6Plw4R0eeH5eFOXxc4hzBys8\nVcSKAuyxuS/B1hZtsqAoR5U7rQUUDg5TSxyPda3s7jM8LCvUfZ55l3GoQQKBgGDa\nvJqaucU/iHKSi2WmGutKLpCDlZikqsydN9XE49K7zHYpKHSXT5PCIIpWPPWnsXCh\nb/wLJEhyytV/rW+vHr8KuRCIVSweNPvtP/mm2DQcYhiXXpd/6aSRTTgAOk3zMj/Y\nw9/YGZC70CV0SOTO1g9179noQ8e7SCnkqITz3xQ/AoGAf6W+vM5glkzb0Rs3Zl0M\nlJoY6D26vQoOwY45Vcu30ClTPceilu/nTeiwkwDlr/yiMuMzrsImJTSb77TMvax/\nAynE9TrvYSvEkgI7gfhSVHB3lSgTKdo/OacXCXfzdOx5wfmX/UkVwl5DcHi+ocFv\ncQ8r3GmAouLJzf7qRJxhzQo=\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-x8086@classmate-classes.iam.gserviceaccount.com",
-  "client_id": "102132813188077862362",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-x8086%40classmate-classes.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
-}
+Environment = autoclass('android.os.Environment')
+path = Environment.getExternalStorageDirectory().getAbsolutePath()
 
-cred = credentials.Certificate(d)
+cred = credentials.Certificate("fireadmin.json")
 firebase_admin.initialize_app(cred, {"storageBucket": "classmate-classes.appspot.com"})
 
 
@@ -43,16 +34,16 @@ class MyData(BoxLayout):
         def download_file(self):
             bucket = storage.bucket()
             blob = bucket.blob("image.jpg")
-            blob.download_to_filename("file/adminimage.jpg")
-            print(f"File {'image.jpg'} downloaded to {'file/adminimage.jpg'}.")
-            w = f"File {'image.jpg'} downloaded to {'file/adminimage.jpg'}."
+            blob.download_to_filename(f"{path}/adminimage.jpg")
+            print(f"File {'image.jpg'} downloaded to {'path/adminimage.jpg'}.")
+            w = f"File {'image.jpg'} downloaded to {'path/adminimage.jpg'}."
             self.ids.notice.text = w
 
     except Exception as e:
         print(e)
 
     def show_image(self):
-        self.source_file = "file/adminimage.jpg"
+        self.source_file = f"{path}/adminimage.jpg"
 
     def delete(self):
         try:
@@ -67,6 +58,17 @@ class MyData(BoxLayout):
 class MyApp(App):
     def build(self):
         return MyData()
+
+    def on_start(self):
+
+        if not check_permission(Permission.READ_EXTERNAL_STORAGE) or not check_permission(
+                Permission.WRITE_EXTERNAL_STORAGE):
+            # Request permissions if not granted
+            permissions = [Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE]
+            request_permissions(permissions)
+        else:
+            # Permissions are already granted, you can proceed with your app logic
+            pass
 
 
 MyApp().run()
