ffdd1a8fc811a605d85b07f83351b1592715891e
Rohit Vishwakarma <126248971+wish-rohitv@users.noreply.github.com>
2023-09-24 11:06:52 +0530

    doc user main.py

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from kivy.properties import StringProperty
 from kivy.uix.boxlayout import BoxLayout
 import firebase_admin
 from firebase_admin import credentials, storage
-from jnius import autoclass
 from android.permissions import check_permission, request_permissions, Permission
+from android import mActivity
 
-Environment = autoclass('android.os.Environment')
-path = Environment.getExternalStorageDirectory().getAbsolutePath()
+context = mActivity.getApplicationContext()
+result = context.getExternalFilesDir("images")   # don't forget the argument
+if result:
+    storage_path = str(result.toString())
+
 
 cred = credentials.Certificate("fireadmin.json")
 firebase_admin.initialize_app(cred, {"storageBucket": "classmate-classes.appspot.com"})
@@ -34,7 +37,7 @@ class MyData(BoxLayout):
         def download_file(self):
             bucket = storage.bucket()
             blob = bucket.blob("image.jpg")
-            blob.download_to_filename(f"{path}/adminimage.jpg")
+            blob.download_to_filename(f"{storage_path}/adminimage.jpg")
             print(f"File {'image.jpg'} downloaded to {'path/adminimage.jpg'}.")
             w = f"File {'image.jpg'} downloaded to {'path/adminimage.jpg'}."
             self.ids.notice.text = w
@@ -43,11 +46,11 @@ class MyData(BoxLayout):
         print(e)
 
     def show_image(self):
-        self.source_file = f"{path}/adminimage.jpg"
+        self.source_file = f"{storage_path}/adminimage.jpg"
 
     def delete(self):
         try:
-            os.remove("file/adminimage.jpg")
+            os.remove(f"{storage_path}/adminimage.jpg")
             print("successfully deleted")
             self.ids.notice.text = "successfully deleted"
         except Exception as g:
